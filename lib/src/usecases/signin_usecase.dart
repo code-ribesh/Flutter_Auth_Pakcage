21cc7ee2582ada974f2baf4270389eb99a5466d8
ribeshbasnet <ribeshbasnet19.rb@gmail.com>
2021-03-09 15:49:42 +0545

    Created SignIn Usecases

---
+++ lib/src/usecases/signin_usecase.dart
@@ -0,0 +1,12 @@
+import 'package:async/async.dart';
+import '../domain/auth_service_contract.dart';
+import '../domain/token.dart';
+
+class SignInUseCase {
+  final IAuthService _authService;
+  SignInUseCase(this._authService);
+
+  Future<Result<Token>> execute() async {
+    return await _authService.signIn();
+  }
+}
